4cd9afd9e600989168430efc61d75b5c5eb3efd9
Ayoub <aragrag.ayoub@gmail.com>
2023-10-13 06:17:43 -0300

    Add more  Doc Comments

--- devoir2/PlusGrandeValeur.java
+++ devoir2/PlusGrandeValeur.java
@@ -1,7 +1,7 @@
 /*
     Titre : PlusGrandeValeur
     Auteur : ARAGRAG AYOUB
-    Date : [Date de création du code]
+    Date : 13-10-2023
     Description: Ce programme génère un tableau de 15 valeurs aléatoires entre 10 et 100,
                  affiche le tableau, identifie la plus grande valeur dans le tableau ainsi que son index.
     Version : 0.0.1
@@ -20,7 +20,7 @@ import java.util.Random;
 public class PlusGrandeValeur {
 
     private List<Integer> valeurs;
-    private static final int TABLEAU_TAILLE = 15;
+    private static final int TABLEAU_TAILLE = 15; // Taille de tableau ou liste
 
     /**
      * Initialise le tableau en fonction du choix de l'utilisateur.
@@ -28,7 +28,7 @@ public class PlusGrandeValeur {
      * @param choice Le choix de l'utilisateur (1 pour ArrayList, 2 pour LinkedList).
      */
     public void initArray(int choice) {
-        Random rand = new Random();
+        Random rand = new Random(); // Pour générer des nombres au hasard
         int max = 100;
         int min = 10;
 
@@ -40,10 +40,11 @@ public class PlusGrandeValeur {
                 this.valeurs = new LinkedList<Integer>();
                 break;
             default:
-                System.out.println("Choix invalide. Sortie du programme.");
+                System.out.println("Choix invalide. Sortie du programme.");// Dans le cas le choix invalide (diffrent de 1, 2 et 0)
                 return;
         }
 
+        // Affichage du tableau avec les indices 
         System.out.println("*****Affichage du tableau*****");
         for (int i = 0; i < TABLEAU_TAILLE; i++) {
             int value = rand.nextInt(max - min + 1) + min;
@@ -56,16 +57,17 @@ public class PlusGrandeValeur {
      * Identifie la plus grande valeur dans le tableau et affiche son index.
      */
     public void maxVal() {
-        int nbrMax = valeurs.get(0);
-        int pos = 0;
+        int nbrMax = valeurs.get(0); // set la 1er valeur du tableau est le max 
+        int pos = 0; // set la position est zero
 
-        for (int i = 1; i < valeurs.size(); i++) {
+        // Ftech Array et recuperer la valeur Max  et  son index
+        for (int i = 1; i < valeurs.size(); i++) { 
             if (nbrMax < valeurs.get(i)) {
                 nbrMax = valeurs.get(i);
                 pos = i;
             }
         }
-        System.out.println("Le plus grand nombre du tableau est : " + nbrMax);
+        System.out.println("Le plus grand nombre du tableau est : " + nbrMax); 
         System.out.println("Il est à l'index : " + pos);
     }
 }
